ab198060590f5ae85e1affc117fad1d6ea59b688
Darya_Savitskaya <statusqvogerz@gmail.com>
2022-09-07 00:10:40 +0500

    L2_T2

--- index.js
+++ index.js
@@ -3,3 +3,9 @@ const mySurname = 'Savitskaya'
 let myFavoriteDrink = 'mohito'
 let myFavoriteAnimal = 'cats'
 let myFavoriteProgrammingLanguage = 'JavaScript'
+
+console.log(myName)
+console.log(mySurname)
+console.log(myFavoriteDrink)
+console.log(myFavoriteAnimal)
+console.log(myFavoriteProgrammingLanguage)
